7b67baca39e532f00a975e765fb8d91bba5415f1
githubscientist <sathish.datascientist@gmail.com>
2023-05-07 10:15:10 +0530

    intial react project setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+// function App() {
+//   return (
+//     <div>
+//       <p>Hello World!</p>
+//     </div>
+//   );
+// }
 
-function App() {
+// lets re-write the javascript function App using arrow syntax
+// const App = () => {
+//   console.log('hello from component');
+//   return (
+//     <div>
+//       <p>Hello World!</p>
+//     </div>
+//   );
+// }
+
+// const App = () => {
+//   const now = new Date();
+//   const a = 10;
+//   const b = 20;
+//   console.log(now, a + b);
+//   return (
+//     <div>
+//       <p>Hello World, it is now {now.toString()}</p>
+//       <p>
+//         {a} plus {b} is {a+b}
+//       </p>
+//     </div>
+//   );
+  // return React.createElement(
+  //   'div',
+  //   null,
+  //   React.createElement(
+  //     'p',
+  //     null,
+  //     'Hello world, it is', now.toString()
+  //   ),
+  //   React.createElement(
+  //     'p',
+  //     null,
+  //     a,
+  //     'plus ', b, 'is ', a+b
+  //   )
+  // );
+// props: passing data to components
+// Hello Component
+const Hello = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  );
+}
+
+// App Component
+const App = () => {
+  const age = 10;
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name='Krish' age={ 26 + 10 } />
+      <Hello name='Sathish' age={age} />
     </div>
   );
 }
