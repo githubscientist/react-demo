a8176148f9baeb4e73e948e9e15877a0a197c45f
githubscientist <sathish.datascientist@gmail.com>
2023-06-03 08:26:58 +0530

    rewind the Router wrap

--- src/App.js
+++ src/App.js
@@ -1477,91 +1477,102 @@ The Profile component should:
 
 // export default App;
 
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-function Button({ value, onClick }) {
-  
+// function Button({ value, onClick }) {
 
+//   return (
+//     <button
+//       className='square'
+//       onClick={onClick}>{value}</button>
+//   )
+// }
 
-  return (
-    <button
-      className='square'
-      onClick={onClick}>{value}</button>
-  )
-}
+// function App() {
 
-function App() {
+//   const [xIsNext, setXIsNext] = useState(true);
+//   const [squares, setSquares] = useState(Array(9).fill(null));
 
-  const [xIsNext, setXIsNext] = useState(true);
-  const [squares, setSquares] = useState(Array(9).fill(null));
+//   function handleClick(i) {
 
-  function handleClick(i) {
+//     if (calculateWinner(squares) || squares[i]) {
+//       return;
+//     }
 
-    if (calculateWinner(squares) || squares[i]) {
-      return;
-    }
+//     const nextSquares = squares.slice();
+//     if (xIsNext) {
+//       nextSquares[i] = 'X';
+//     } else {
+//       nextSquares[i] = 'O';
+//     }
+//     setSquares(nextSquares);
+//     setXIsNext(!xIsNext);
+//   }
 
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
-  }
+//   function calculateWinner(squares) {
+//     const lines = [
+//       [0, 1, 2],
+//       [3, 4, 5],
+//       [6, 7, 8],
+//       [0, 3, 6],
+//       [1, 4, 7],
+//       [2, 5, 8],
+//       [0, 4, 8],
+//       [2, 4, 6],
+//     ];
 
-  function calculateWinner(squares) {
-    const lines = [
-      [0, 1, 2],
-      [3, 4, 5],
-      [6, 7, 8],
-      [0, 3, 6],
-      [1, 4, 7],
-      [2, 5, 8],
-      [0, 4, 8],
-      [2, 4, 6],
-    ];
+//     for (let i = 0; i < lines.length; i++){
+//       const [a, b, c] = lines[i];
+//       if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+//         return squares[a];
+//       }
+//     }
+//     return null;
+//   }
 
-    for (let i = 0; i < lines.length; i++){
-      const [a, b, c] = lines[i];
-      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-        return squares[a];
-      }
-    }
-    return null;
-  }
-
-  const winner = calculateWinner(squares);
-  let status;
-  if (winner) {
-    status = 'Winner: ' + winner;
-  } else {
-    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
-  }
+//   const winner = calculateWinner(squares);
+//   let status;
+//   if (winner) {
+//     status = 'Winner: ' + winner;
+//   } else {
+//     status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+//   }
     
 
+//   return (
+//     <div>
+//       <div>
+//         {status}
+//       </div>
+//       <div className='row'>
+//         <Button value={squares[0]} onClick={ () => handleClick(0) } />
+//         <Button value={squares[1]} onClick={ () => handleClick(1) } />
+//        <Button value={squares[2]} onClick={ () => handleClick(2) } />
+//       </div>
+//       <div className='row'>
+//         <Button value={squares[3]} onClick={ () => handleClick(3) } />
+//         <Button value={squares[4]} onClick={ () => handleClick(4) } />
+//        <Button value={squares[5]} onClick={ () => handleClick(5) } />
+//       </div>
+//       <div className='row'>
+//         <Button value={squares[6]} onClick={ () => handleClick(6) } />
+//         <Button value={squares[7]} onClick={ () => handleClick(7) } />
+//        <Button value={squares[8]} onClick={ () => handleClick(8) } />
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// Note Taking Application - CRUD Operations
+// Create, Read, Update, Delete
+
+import React from 'react';
+
+function App() {
   return (
-    <div>
-      <div>
-        {status}
-      </div>
-      <div className='row'>
-        <Button value={squares[0]} onClick={ () => handleClick(0) } />
-        <Button value={squares[1]} onClick={ () => handleClick(1) } />
-       <Button value={squares[2]} onClick={ () => handleClick(2) } />
-      </div>
-      <div className='row'>
-        <Button value={squares[3]} onClick={ () => handleClick(3) } />
-        <Button value={squares[4]} onClick={ () => handleClick(4) } />
-       <Button value={squares[5]} onClick={ () => handleClick(5) } />
-      </div>
-      <div className='row'>
-        <Button value={squares[6]} onClick={ () => handleClick(6) } />
-        <Button value={squares[7]} onClick={ () => handleClick(7) } />
-       <Button value={squares[8]} onClick={ () => handleClick(8) } />
-      </div>
-    </div>
+    <div>App</div>
   )
 }
 
