88e80c0b38e32f5b74454f0ae090822a2191c631
githubscientist <sathish.datascientist@gmail.com>
2023-06-03 08:30:22 +0530

    pass notes as props to App

--- src/index.js
+++ src/index.js
@@ -26,7 +26,7 @@ const notes = [
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App notes={ notes } />
   </React.StrictMode>
   // <BrowserRouter>
   //   <App />
