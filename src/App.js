ffa38b21e835b401d0ff7a54bc7293421ea492d0
githubscientist <sathish.datascientist@gmail.com>
2023-06-04 10:30:55 +0530

    redux

--- src/App.js
+++ src/App.js
@@ -1568,131 +1568,186 @@ The Profile component should:
 // Note Taking Application - CRUD Operations
 // Create, Read, Update, Delete
 
-import React, { useEffect, useState } from 'react';
-import Note from './components/Note';
-import axios from 'axios';
+// import React, { useEffect, useState } from 'react';
+// import Note from './components/Note';
+// import axios from 'axios';
+
+// function App() {
+//   const [notes, setNotes] = useState([]);
+//   const [newNote, setNewNote] = useState('');
+//   const [showAll, setShowAll] = useState(true);
+
+//   // make the api request
+//   useEffect(() => {
+//     axios
+//       .get('http://localhost:3002/notes')
+//       .then(response => {
+//         setNotes(response.data);
+//       })
+//   }, []);
+
+//   const notesToShow = showAll ? notes : notes.filter(note => note.important === true);
+
+//   // addNote function
+//   const addNote = (event) => {
+//     event.preventDefault();
+//     // console.log('button clicked', event.target);
+    
+//     // create the new note object
+//     const noteObject = {
+//       content: newNote,
+//       important: Math.random() < 0.5,
+//     };
+
+//     // append it to the notes state
+//     // setNotes(notes.concat(noteObject));
+//     axios
+//       .post('http://localhost:3002/notes', noteObject)
+//       .then(response => {
+//         // console.log(response);
+//         // after a creating a new resource
+//         // update the state
+//         setNotes(notes.concat(response.data));
+//         setNewNote('');
+//       });
+
+//     // clear the newNote state
+//     setNewNote('');
+//   }
+
+//   // handleNoteChange handler function
+//   const handleNoteChange = (event) => {
+//     // console.log(event.target.value);
+//     setNewNote(event.target.value);
+//   }
+
+//   // axios demo
+//   // const promise = axios.get('http://localhost:3002/notes');
+//   // console.log(promise);
+//   // const promise = axios.get('http://localhost:3002/foobar');
+//   // console.log(promise);
+
+//   // const promise = axios.get('http://localhost:3002/notes');
+
+//   // promise.then(response => {
+//   //   const notes = response.data;
+//   // });
+
+//   const toggleImportanceOf = (id) => {
+//     // console.log('importance of ' + id + ' needs to be toggled');
+//     const url = `http://localhost:3002/notes/${id}`;
+//     const note = notes.find(n => n.id === id);
+//     const changedNote = {
+//       ...note,
+//       important: !note.important
+//     };
+
+//     // make the api request
+//     axios
+//       .put(url, changedNote)
+//       .then(response => {
+//         setNotes(notes.map(note => {
+//           // return the entire original array or
+//           // the updated
+//           return note.id !== id ? note : response.data;
+//         }));
+//       });
+//   }
+
+//   const deleteNoteHandler = (id) => {
+//     // console.log(id);
+//     const url = `http://localhost:3002/notes/${id}`;
+//     axios
+//       .delete(url)
+//       .then(response => {
+//         setNotes(notes.filter(note => {
+//           return note.id !== id;
+//         }))
+//       })
+//   }
+
+//   return (
+//     <div>
+//       <h1>Notes</h1>
+//       <div>
+//         <button onClick={() => setShowAll(!showAll)}>
+//           show {showAll ? 'important' : 'all'}
+//         </button>
+//       </div>
+//       <ul>
+//         {
+//           notesToShow.map(
+//             note => <Note
+//               key={note.id}
+//               note={note} 
+//               toggleImportance={() => toggleImportanceOf(note.id)}
+//               deleteNote={() => deleteNoteHandler(note.id)}
+//               />
+//           )
+//         }
+//       </ul>
+
+//       <form onSubmit={addNote}>
+//         <input value={ newNote } onChange={handleNoteChange} placeholder='a new note...'/>
+//         <button type='submit'>save</button>
+//       </form>
+
+//       {/* redux - state management */}
+//     </div>
+//   )
+// }
+
+// export default App;
+
+import React from 'react';
+import { createStore } from 'redux';
+
+// Reducer function
+const counterReducer = (state = 0, action) => {
+  // if (action.type === 'INCREMENT') {
+  //   return state + 1;
+  // } else if (action.type === 'DECREMENT') {
+  //   return state - 1;
+  // } else if (action.type === 'ZERO') {
+  //   return 0;
+  // }
+  // return state;
+
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    case 'ZERO':
+      return 0;
+    default:
+      return state;
+  }
+}
+
+// create a new store
+const store = createStore(counterReducer);
 
 function App() {
-  const [notes, setNotes] = useState([]);
-  const [newNote, setNewNote] = useState('');
-  const [showAll, setShowAll] = useState(true);
-
-  // make the api request
-  useEffect(() => {
-    axios
-      .get('http://localhost:3002/notes')
-      .then(response => {
-        setNotes(response.data);
-      })
-  }, []);
-
-  const notesToShow = showAll ? notes : notes.filter(note => note.important === true);
-
-  // addNote function
-  const addNote = (event) => {
-    event.preventDefault();
-    // console.log('button clicked', event.target);
-    
-    // create the new note object
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-    };
-
-    // append it to the notes state
-    // setNotes(notes.concat(noteObject));
-    axios
-      .post('http://localhost:3002/notes', noteObject)
-      .then(response => {
-        // console.log(response);
-        // after a creating a new resource
-        // update the state
-        setNotes(notes.concat(response.data));
-        setNewNote('');
-      });
-
-    // clear the newNote state
-    setNewNote('');
-  }
-
-  // handleNoteChange handler function
-  const handleNoteChange = (event) => {
-    // console.log(event.target.value);
-    setNewNote(event.target.value);
-  }
-
-  // axios demo
-  // const promise = axios.get('http://localhost:3002/notes');
-  // console.log(promise);
-  // const promise = axios.get('http://localhost:3002/foobar');
-  // console.log(promise);
-
-  // const promise = axios.get('http://localhost:3002/notes');
-
-  // promise.then(response => {
-  //   const notes = response.data;
-  // });
-
-  const toggleImportanceOf = (id) => {
-    // console.log('importance of ' + id + ' needs to be toggled');
-    const url = `http://localhost:3002/notes/${id}`;
-    const note = notes.find(n => n.id === id);
-    const changedNote = {
-      ...note,
-      important: !note.important
-    };
-
-    // make the api request
-    axios
-      .put(url, changedNote)
-      .then(response => {
-        setNotes(notes.map(note => {
-          // return the entire original array or
-          // the updated
-          return note.id !== id ? note : response.data;
-        }));
-      });
-  }
-
-  const deleteNoteHandler = (id) => {
-    // console.log(id);
-    const url = `http://localhost:3002/notes/${id}`;
-    axios
-      .delete(url)
-      .then(response => {
-        setNotes(notes.filter(note => {
-          return note.id !== id;
-        }))
-      })
-  }
 
   return (
     <div>
-      <h1>Notes</h1>
+
       <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
-        </button>
+        {store.getState()}
       </div>
-      <ul>
-        {
-          notesToShow.map(
-            note => <Note
-              key={note.id}
-              note={note} 
-              toggleImportance={() => toggleImportanceOf(note.id)}
-              deleteNote={() => deleteNoteHandler(note.id)}
-              />
-          )
-        }
-      </ul>
 
-      <form onSubmit={addNote}>
-        <input value={ newNote } onChange={handleNoteChange} placeholder='a new note...'/>
-        <button type='submit'>save</button>
-      </form>
+      <button onClick={e => store.dispatch({type: 'INCREMENT'})}>
+        plus
+      </button>
 
-      {/* redux - state management */}
+      <button onClick={e => store.dispatch({type: 'DECREMENT'})}>
+        minus
+      </button>
+
+      <button onClick={e => store.dispatch({type: 'ZERO'})}>
+        zero
+      </button>
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 // import { BrowserRouter } from 'react-router-dom';
 
 // add some data
@@ -23,16 +22,70 @@ import App from './App';
 //   }
 // ];
 
+import { createStore } from 'redux';
+
+// Reducer function
+const counterReducer = (state = 0, action) => {
+  // if (action.type === 'INCREMENT') {
+  //   return state + 1;
+  // } else if (action.type === 'DECREMENT') {
+  //   return state - 1;
+  // } else if (action.type === 'ZERO') {
+  //   return 0;
+  // }
+  // return state;
+
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1;
+    case 'DECREMENT':
+      return state - 1;
+    case 'ZERO':
+      return 0;
+    default:
+      return state;
+  }
+}
+
+// create a new store
+const store = createStore(counterReducer);
+
+function App() {
+
+  return (
+    <div>
+
+      <div>
+        {store.getState()}
+      </div>
+
+      <button onClick={e => store.dispatch({type: 'INCREMENT'})}>
+        plus
+      </button>
+
+      <button onClick={e => store.dispatch({type: 'DECREMENT'})}>
+        minus
+      </button>
+
+      <button onClick={e => store.dispatch({type: 'ZERO'})}>
+        zero
+      </button>
+    </div>
+  )
+}
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    {/* <App notes={ notes } /> */}
-    <App />
-  </React.StrictMode>
-  // <BrowserRouter>
-  //   <App />
-  // </BrowserRouter>
-);
+
+const renderApp = () => {
+  root.render(
+  <App />
+)
+}
+
+renderApp();
+
+store.subscribe(renderApp);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
