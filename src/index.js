92932c17cf190750fc34bba5762c40c37aaeba33
githubscientist <sathish.datascientist@gmail.com>
2023-07-09 09:04:23 +0530

    crud with react

--- src/index.js
+++ src/index.js
@@ -2,38 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// import { BrowserRouter } from 'react-router-dom';
-
-// add some data
-// const notes = [
-//   {
-//     id: 1,
-//     content: 'HTML is easy',
-//     important: true,
-//   },
-//   {
-//     id: 2,
-//     content: 'Browser can execute only JavaScript',
-//     important: false,
-//   },
-//   {
-//     id: 3,
-//     content: 'GET and POST are the most important methods of HTTP protocol',
-//     important: true,
-//   }
-// ];
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    {/* <App notes={ notes } /> */}
     <App />
   </React.StrictMode>
-  // <BrowserRouter>
-  //   <App />
-  // </BrowserRouter>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
